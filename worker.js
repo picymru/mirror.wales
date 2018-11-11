53cb590cd1c4e7eca0928c63373d2af57d728c14
Matthew Gall <git@matthewgall.com>
2018-11-11 16:39:37 +0000

    Added product identification by subdomain

--- worker.js
+++ worker.js
@@ -11,9 +11,24 @@ function isInArray(value, array) {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  path = new URL(request.url).pathname;
 
-  return new Response('Welcome to mirror.wales', {
+  const PRODUCT_NAME = "mirror.wales"
+
+  let host = new URL(request.url).host
+  let path = new URL(request.url).pathname;
+
+  // Now we do some syntax checking to ensure the host is valid
+  if (host.includes(`.${PRODUCT_NAME}`) == false) {
+    return new Response(`You requested an invalid host. Please visit our website at ${PRODUCT_NAME}`, {
+      headers: {
+        'Content-Type': 'text/plain'
+      }
+    })
+  }
+
+  // Now we fetch the subdomain from the host
+  let product = host.replace(`.${PRODUCT_NAME}`, '')
+  return new Response(product, {
     headers: {
       'Content-Type': 'text/plain'
     }
